f515b0051b74c69e8876a147ee39c115706f0afe
Vasil <vastril4o@gmail.com>
2018-02-11 00:44:17 +0200

    FEAT > add particles-js to header

---
+++ res/js/main.js
@@ -0,0 +1,4 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', './res/json/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
